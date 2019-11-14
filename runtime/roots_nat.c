e955ba4aa4b35210cef4c71d4494b385b2631cb6
Enguerrand Decorne <decorne.en@gmail.com>
2019-11-14 15:48:45 +0100

    eventlog: roots in native backend were missing a few events

--- runtime/roots_nat.c
+++ runtime/roots_nat.c
@@ -27,6 +27,7 @@
 #include "caml/stack.h"
 #include "caml/roots.h"
 #include "caml/memprof.h"
+#include "caml/eventlog.h"
 #include <string.h>
 #include <stdio.h>
 
@@ -352,6 +353,7 @@ intnat caml_darken_all_roots_slice (intnat work)
   static mlsize_t roots_count = 0;
   intnat remaining_work = work;
   CAML_INSTR_SETUP (tmr, "");
+  caml_ev_begin(EV_MAJOR_MARK_GLOBAL_ROOTS_SLICE);
 
   /* If the loop was started in a previous call, resume it. */
   if (do_resume) goto resume;
@@ -381,6 +383,7 @@ intnat caml_darken_all_roots_slice (intnat work)
 
  suspend:
   /* Do this in both cases. */
+  caml_ev_end(EV_MAJOR_MARK_GLOBAL_ROOTS_SLICE);
   CAML_INSTR_TIME (tmr, "major/mark/global_roots_slice");
   return remaining_work;
 }
@@ -392,6 +395,7 @@ void caml_do_roots (scanning_action f, int do_globals)
   link *lnk;
   CAML_INSTR_SETUP (tmr, "major_roots");
 
+  caml_ev_begin(EV_MAJOR_ROOTS_DYNAMIC_GLOBAL);
   if (do_globals){
     /* The global roots */
     for (i = 0; caml_globals[i] != 0; i++) {
@@ -410,22 +414,33 @@ void caml_do_roots (scanning_action f, int do_globals)
     }
   }
   CAML_INSTR_TIME (tmr, "major_roots/dynamic_global");
+  caml_ev_end(EV_MAJOR_ROOTS_DYNAMIC_GLOBAL);
   /* The stack and local roots */
+  caml_ev_begin(EV_MAJOR_ROOTS_LOCAL);
   caml_do_local_roots(f, Caml_state->bottom_of_stack,
                       Caml_state->last_return_address, Caml_state->gc_regs,
                       Caml_state->local_roots);
+  caml_ev_end(EV_MAJOR_ROOTS_LOCAL);
   CAML_INSTR_TIME (tmr, "major_roots/local");
   /* Global C roots */
+  caml_ev_begin(EV_MAJOR_ROOTS_C);
   caml_scan_global_roots(f);
   CAML_INSTR_TIME (tmr, "major_roots/C");
+  caml_ev_end(EV_MAJOR_ROOTS_C);
   /* Finalised values */
+  caml_ev_begin(EV_MAJOR_ROOTS_FINALISED);
   caml_final_do_roots (f);
+  caml_ev_end(EV_MAJOR_ROOTS_FINALISED);
   CAML_INSTR_TIME (tmr, "major_roots/finalised");
   /* Memprof */
+  caml_ev_begin(EV_MAJOR_ROOTS_MEMPROF);
   caml_memprof_scan_roots (f);
+  caml_ev_end(EV_MAJOR_ROOTS_MEMPROF);
   CAML_INSTR_TIME (tmr, "major_roots/memprof");
   /* Hook */
+  caml_ev_begin(EV_MAJOR_ROOTS_HOOK);
   if (caml_scan_roots_hook != NULL) (*caml_scan_roots_hook)(f);
+  caml_ev_end(EV_MAJOR_ROOTS_HOOK);
   CAML_INSTR_TIME (tmr, "major_roots/hook");
 }
 
