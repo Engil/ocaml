8634897cf146fc392d71d37ad4014457300c896b
Enguerrand Decorne <decorne.en@gmail.com>
2019-11-15 13:52:35 +0100

    Rewrite the instrumented runtime to store traces in the CTF format.
    The instrumentation code in the instrumented runtime was replaced
    with new APIs to gather runtime statistics and output them in a new format
    (Common Trace Format).
    This commit also exposes new functions in the Gc module to pause or resume
    instrumentation during a program execution (Gc.eventlog_pause and
    Gc.eventlog_resume).

--- runtime/memprof.c
+++ runtime/memprof.c
@@ -779,7 +779,6 @@ void caml_memprof_track_young(uintnat wosize, int from_caml,
   /* We can now restore the minor heap in the state needed by
      [Alloc_small_aux]. */
   if (Caml_state->young_ptr - whsize < Caml_state->young_trigger) {
-    CAML_INSTR_INT("force_minor/memprof@", 1);
     caml_gc_dispatch();
   }
 
