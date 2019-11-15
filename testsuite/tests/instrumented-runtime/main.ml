4018bf243ad998ee1fce506a302fc6a2a4754251
Enguerrand Decorne <decorne.en@gmail.com>
2019-11-15 13:52:35 +0100

    Rewrite the instrumented runtime to store traces in the CTF format.
    The instrumentation code in the instrumented runtime was replaced
    with new APIs to gather runtime statistics and output them in a new format
    (Common Trace Format).
    This commit also exposes new functions in the Gc module to pause or resume
    instrumentation during a program execution (Gc.eventlog_pause and
    Gc.eventlog_resume).

---
+++ testsuite/tests/instrumented-runtime/main.ml
@@ -0,0 +1,12 @@
+(* TEST
+  * hasinstrumentedruntime
+  ** native
+    flags = "-runtime-variant=i"
+*)
+
+(* Test if the instrumented runtime is in working condition *)
+
+let _ =
+  Gc.eventlog_pause ();
+  Gc.eventlog_resume();
+  print_endline "OK"
