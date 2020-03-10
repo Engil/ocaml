21334da84ed9dafe14acd587aacf31d1e268bb49
Enguerrand Decorne <decorne.en@gmail.com>
2020-03-10 12:48:11 +0100

    eventlog: add author header in eventlog.c and eventlog.h

--- runtime/caml/eventlog.h
+++ runtime/caml/eventlog.h
@@ -1,3 +1,19 @@
+/**************************************************************************/
+/*                                                                        */
+/*                                 OCaml                                  */
+/*                                                                        */
+/*                 Stephen Dolan, University of Cambridge                 */
+/*                      Enguerrand Decorne, Tarides                       */
+/*                                                                        */
+/*   Copyright 2020 University of Cambridge                               */
+/*   Copyright 2020 Tarides                                               */
+/*                                                                        */
+/*   All rights reserved.  This file is distributed under the terms of    */
+/*   the GNU Lesser General Public License version 2.1, with the          */
+/*   special exception on linking described in the file LICENSE.          */
+/*                                                                        */
+/**************************************************************************/
+
 #ifndef CAML_EVENTLOG_H
 #define CAML_EVENTLOG_H
 
